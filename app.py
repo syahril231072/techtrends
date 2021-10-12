8515259223f1c10181b3ef4222bb4781508cccc6
syahril ramadhan <syahril231072@gmail.com>
2021-10-12 15:27:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,10 @@ def page_not_found(e):
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    handlers = [stderr_handler, stdout_handler]
+    # format output
+    format_output = '%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+    logging.basicConfig(level=logging.DEBUG, handlers=handlers, format=format_output)
     app.run(host='0.0.0.0', port='3111')
